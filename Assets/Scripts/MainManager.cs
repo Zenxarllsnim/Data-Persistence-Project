f40aad9d166de3f6ab781608fdab44a22ee7fc26
Zenxarllsnim <85424096+Zenxarllsnim@users.noreply.github.com>
2021-09-02 18:35:33 -0700

    Finish Commit
    Everything works as expected, though it could have been differently... I atleast understand how this way works hehe.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -11,17 +12,33 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text Name;
+    public GameObject NameInput;
     public GameObject GameOverText;
-    
+    public GameObject GameOverTextHighScore;
+    public Text HighScore;
+    public static MainManager Instance;
+
+    private bool highflag = false;
     private bool m_Started = false;
     private int m_Points;
+    private int high_score;
+    private string highname;
     
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        LoadScore();
+      
+    }
     void Start()
     {
+        
+        UpdateHighScore();
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -55,8 +72,22 @@ public class MainManager : MonoBehaviour
         }
         else if (m_GameOver)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+             if (Input.GetKeyDown(KeyCode.Home))
             {
+                high_score = 0;
+                highname = "Reset";
+                UpdateHighScore();
+                SaveScore();
+                
+            }
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                highname = Name.text;
+                NameInput.SetActive(false);
+                UpdateHighScore();
+                SaveScore();
+                
+                        
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
@@ -66,11 +97,70 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+        if (m_Points >= high_score)
+        {
+            high_score = m_Points;
+                        
+            highflag = true;
+        }
     }
 
+
+    public void UpdateHighScore()
+    {
+        if (highname != null) {
+            HighScore.text = "HighScore: " + high_score + " by " + highname;
+        }
+        else
+        { HighScore.text = "HighScore: " + high_score; }
+
+    }
     public void GameOver()
     {
         m_GameOver = true;
-        GameOverText.SetActive(true);
+        if (highflag == false)
+        {
+            GameOverText.SetActive(true);
+        }
+        else if (highflag == true)
+        {
+            GameOverTextHighScore.SetActive(true);
+            NameInput.SetActive(true);
+            
+            
+            SaveScore();
+           
+        }
     }
+    class SaveData
+    {
+        public int highscoresave;
+        public string highname;
+    }
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.highscoresave = high_score;
+        data.highname = highname;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            high_score = data.highscoresave;
+            highname = data.highname;
+        }
+    }
+
+
+
 }
